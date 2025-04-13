a9445975d967bbe337867c053daaa429e7190805
Mohamed Esam <moesam138@gmail.com>
2025-04-13 19:17:12 +0200

    [217] [Time Beats: 82.53%] [Memory Beats: 51.02%] - LeetPush

--- Contains-Duplicate.kt
+++ Contains-Duplicate.kt
@@ -1,13 +1,11 @@
 class Solution {
    fun containsDuplicate(nums: IntArray): Boolean {
-    val maxNumber = nums.maxOrNull() ?: 0
-    val map = mutableMapOf<Int, Int>()
-    for (index in nums.indices) {
-        val diff = maxNumber - nums[index]
-        if (map.containsKey(diff)) {
+    val seen = mutableSetOf<Int>()
+    for (num in nums) {
+        if (num in seen) {
             return true
         }
-        map[diff] = index
+        seen.add(num)
     }
     return false
 }
