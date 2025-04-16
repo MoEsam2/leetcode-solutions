08f74e01a75f4e80b1b6bdc3dc4bb09f78b8e226
Mohamed Esam <moesam138@gmail.com>
2025-04-16 22:49:15 +0200

    [125] [Time Beats: 63.65%] [Memory Beats: 28.86%] - LeetPush

--- Valid-Palindrome.kt
+++ Valid-Palindrome.kt
@@ -1,16 +1,24 @@
 class Solution {
    fun isPalindrome(s: String): Boolean {
-    val cleanedString = s.lowercase().replace(Regex("[^a-z0-9]"), "")
-    println(cleanedString)
-    var right = cleanedString.length-1
+    var right = s.length-1
     var left = 0
     while (left < right) {
-        if (cleanedString[left] != cleanedString[right]) {
+       if (left < right && !s[left].isLetterOrDigit()){
+           left++
+           continue
+       }
+        if (left < right && !s[right].isLetterOrDigit()){
+            right--
+            continue
+        }
+        if (s[left].lowercase() != s[right].lowercase()){
             return false
         }
-        left++
         right--
+        left++
+
     }
     return true
 }
+
 }
