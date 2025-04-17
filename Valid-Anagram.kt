37293fe041375bb85d48dc3f546fe04b9e6e6f9d
Mohamed Esam <moesam138@gmail.com>
2025-04-17 23:29:34 +0200

    [242] [Time Beats: 42.35%] [Memory Beats: 14.56%] - LeetPush

--- Valid-Anagram.kt
+++ Valid-Anagram.kt
@@ -1,7 +1,23 @@
 class Solution {
  fun isAnagram(s: String, t: String): Boolean {
-    val firstWord = s.toList().sorted()
-    val secondWord = t.toList().sorted()
+    val firstWord = mutableMapOf<Char, Int>()
+    val secondWord = mutableMapOf<Char, Int>()
+    for (index in s.indices) {
+        var key = s[index]
+        if (!firstWord.containsKey(key)) {
+            firstWord[key] = 0
+        } else {
+            firstWord[key] = firstWord[key]!! + 1
+        }
+    }
+    for (index in t.indices) {
+        var key = t[index]
+        if (!secondWord.containsKey(key)) {
+            secondWord[key] = 0
+        } else {
+            secondWord[key] = secondWord[key]!! + 1
+        }
+    }
     return firstWord == secondWord
 }
 }
