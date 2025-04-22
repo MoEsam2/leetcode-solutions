d09008dbe79a21c69a6a564cf5de97aa0190b39c
Mohamed Esam <moesam138@gmail.com>
2025-04-22 04:50:10 +0200

    [283] [Time Beats: 91.11%] [Memory Beats: 57.26%] - LeetPush

--- Move-Zeroes.kt
+++ Move-Zeroes.kt
@@ -1,24 +1,14 @@
 class Solution {
-   fun moveZeroes(nums: IntArray): Unit {
-    var left = 0
-    var right = 1
-    if (nums.size == 1) {
-        println(nums)
-        return
-    }
-    while (right != nums.size) {
-        if (nums[left] == 0 && nums[right] != 0) {
-            val temp = nums[left]
-            nums[left] = nums[right]
-            nums[right] = temp
-            left++
-            right++
-        } else if (nums[left] == 0 && nums[right] == 0) {
-            right++
-        } else {
-            left++
-            right++
+ fun moveZeroes(nums: IntArray): Unit {
+    var pointerToZero = 0
+    for (i in nums.indices) {
+        if (nums[i] != 0) {
+            val temp = nums[i]
+            nums[i] = nums[pointerToZero]
+            nums[pointerToZero] = temp
+            pointerToZero++
         }
     }
 }
+
 }
