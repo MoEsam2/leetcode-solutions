fc01dbe2f5f4e23a33bd0e276cad415a21d276ca
Mohamed Esam <moesam138@gmail.com>
2025-04-24 15:04:20 +0200

    [347] [Time Beats: 49.27%] [Memory Beats: 5.54%] - LeetPush

--- Top-K-Frequent-Elements.kt
+++ Top-K-Frequent-Elements.kt
@@ -1,11 +1,13 @@
 class Solution {
-    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+
+fun topKFrequent(nums: IntArray, k: Int): IntArray {
     val map = mutableMapOf<Int, Int>()
     nums.forEach { num ->
         map[num] = map.getOrDefault(num, 0) + 1
     }
-    val maxValues = map.entries.sortedByDescending { it.value }.take(k)
-    val result = maxValues.map { it.key }.toIntArray()
-    return result
+    val freq = Array<MutableList<Int>>(nums.size + 1) { mutableListOf() }
+    map.forEach { k,v -> freq[v].add(k) }
+    return freq.flatMap { it }.takeLast(k).toIntArray()
 }
+
 }
