8a2a9533bc1a36094eaf89a82e9a2cd74d67f0f3
Mohamed Esam <moesam138@gmail.com>
2025-09-25 04:09:02 +0300

    [13] [Time Beats: 25.57%] [Memory Beats: 12.47%] - LeetPush

--- Roman-to-Integer.kt
+++ Roman-to-Integer.kt
@@ -1,7 +1,5 @@
 class Solution {
- fun romanToInt(s: String): Int {
-    var result = 0
-    val size = s.length
+fun romanToInt(s: String): Int {
     val numeralsTable = mapOf(
         'I' to 1,
         'V' to 5,
@@ -11,15 +9,14 @@ class Solution {
         'D' to 500,
         'M' to 1000
     )
-    s.forEachIndexed { index, c ->
-        val value = numeralsTable.getOrDefault(c,0)
+    return s.foldIndexed(0) { index, acc, c ->
+        val value = numeralsTable.getOrDefault(c, 0)
         val nextValue = numeralsTable.getOrDefault(s.getOrNull(index + 1), 0)
-        if (value < nextValue) {
-            result += (-value)
-        } else {
-            result += value
-        }
+        if (value < nextValue)
+            acc - value
+        else
+            acc + value
+
     }
-    return result
 }
 }
