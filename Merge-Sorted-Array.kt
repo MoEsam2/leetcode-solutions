ab39d203e1242dc43f80a801eeac65e0371d108e
Mohamed Esam <moesam138@gmail.com>
2025-10-07 02:50:12 +0300

    [88] [Time Beats: 100.00%] [Memory Beats: 80.65%] - LeetPush

--- Merge-Sorted-Array.kt
+++ Merge-Sorted-Array.kt
@@ -1,13 +1,12 @@
 class Solution {
 fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
-    val size = m + n
     val result = IntArray(nums1.size)
     for (i in nums1.indices) {
         result[i] = nums1[i]
     }
     var right = 0
     var left = 0
-    for (i in 0 until size) {
+    for (i in nums1.indices) {
         if (right == n || (result[left] <= nums2[right] && !(left >= m))) {
             nums1[i] = result[left]
             left++
