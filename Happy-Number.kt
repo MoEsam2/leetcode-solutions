0692942602217d377eaed5864cfd534f9d188d92
Mohamed Esam <moesam138@gmail.com>
2025-10-07 19:28:55 +0300

    [202] [Time Beats: 29.18%] [Memory Beats: 39.51%] - LeetPush

--- Happy-Number.kt
+++ Happy-Number.kt
@@ -3,19 +3,13 @@ fun isHappy(n: Int): Boolean {
     var str = n.toString()
     val tab = mutableMapOf<String, Int>()
     while (!tab.containsKey(str)) {
-        println(str)
         tab[str] = 0
         if (str == "1") return true
-        if (str.length == 1) str = ((str.toInt()) * (str.toInt())).toString()
-        else {
-            var num = 0
-            str.forEach {
-                num += (it.digitToInt() * it.digitToInt())
-            }
-            str = num.toString()
-
+        var num = 0
+        str.forEach {
+            num += (it.digitToInt() * it.digitToInt())
         }
-
+        str = num.toString()
     }
     return false
 }
