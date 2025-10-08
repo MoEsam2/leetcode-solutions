d928e584e1470dcc6b9b9754c46fa8c4a2f8c675
Mohamed Esam <moesam138@gmail.com>
2025-10-08 17:13:41 +0300

    [125] [Time Beats: 99.87%] [Memory Beats: 78.14%] - LeetPush

--- NeetCode 150/Valid-Palindrome.kt
+++ NeetCode 150/Valid-Palindrome.kt
@@ -1,13 +1,19 @@
 class Solution {
 fun isPalindrome(s: String): Boolean {
+    fun toLower(c: Char): Char {
+        return if (c in 'A'..'Z') (c + 32) else c
+    }
+
+    fun isAlphaNum(c: Char): Boolean {
+        return (c in 'a'..'z') || (c in 'A'..'Z') || (c in '0'..'9')
+    }
+
     var left = 0
     var right = s.length - 1
     while (left < right) {
-        while (!s[left].isLetterOrDigit() && left != right) left++
-        while (!s[right].isLetterOrDigit() && right != left) right--
-        val leftChar = s[left].lowercaseChar()
-        val rightChar = s[right].lowercaseChar()
-        if (leftChar != rightChar) return false
+        while (!isAlphaNum(s[left]) && left != right) left++
+        while (!isAlphaNum(s[right]) && right != left) right--
+        if (toLower(s[left]) != toLower(s[right])) return false
         left++
         right--
     }
