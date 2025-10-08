f6343727fd4664e4a75f394376d6b70bb259ae4b
Mohamed Esam <moesam138@gmail.com>
2025-10-08 16:44:52 +0300

    [125] [Time Beats: 52.77%] [Memory Beats: 54.49%] - LeetPush

---
+++ NeetCode 150/Valid-Palindrome.kt
@@ -0,0 +1,22 @@
+class Solution {
+   fun isPalindrome(s: String): Boolean {
+    var right = s.length-1
+    var left = 0
+    while (left < right) {
+       if (!s[left].isLetterOrDigit()){
+           left++
+           continue
+       }
+        if (!s[right].isLetterOrDigit()){
+            right--
+            continue
+        }
+        if (s[left].lowercase() != s[right].lowercase()){
+            return false
+        }
+        right--
+        left++
+    }
+    return true
+}
+}
