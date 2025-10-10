c1da9b19ac4e169cad1574dce25524d68dda698e
Mohamed Esam <moesam138@gmail.com>
2025-10-10 18:50:17 +0300

    [49] [Time Beats: 44.06%] [Memory Beats: 50.04%] - LeetPush

--- NeetCode 150/Group-Anagrams.kt
+++ NeetCode 150/Group-Anagrams.kt
@@ -1,16 +1,12 @@
 class Solution {
-  fun groupAnagrams(strs: Array<String>): List<List<String>> {
+fun groupAnagrams(strs: Array<String>): List<List<String>> {
     val seenAnagrams = mutableMapOf<String, MutableList<String>>()
     for (str in strs) {
         val currentKey = str.toCharArray().sorted().toString()
         if (!seenAnagrams.containsKey(currentKey)) seenAnagrams[currentKey] = mutableListOf(str)
         else seenAnagrams[currentKey]?.add(str)
     }
-    val groupOfAnagrams = mutableListOf<List<String>>()
-    seenAnagrams.forEach {
-        groupOfAnagrams.add(it.value)
-    }
-    return groupOfAnagrams
+    return seenAnagrams.values.toList()
 }
  
 }
