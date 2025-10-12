e30303ffe31997f213562d96a4b80cdb161896f0
Mohamed Esam <moesam138@gmail.com>
2025-10-12 22:09:55 +0300

    [567] [Time Beats: 11.20%] [Memory Beats: 5.47%] - LeetPush

--- sliding-window/Permutation-in-String.kt
+++ sliding-window/Permutation-in-String.kt
@@ -1,9 +1,15 @@
 class Solution {
 fun checkInclusion(s1: String, s2: String): Boolean {
-    val sortedString = s1.toCharArray().sorted()
-    println(sortedString)
-    for (i in 0 .. s2.length-s1.length) {
-        if (s2.substring(i, i + s1.length).toCharArray().sorted() == sortedString) return true
+    val strArray = IntArray(26)
+    s1.forEach {
+        strArray[it - 'a'] += 1
+    }
+    for (i in 0..s2.length - s1.length) {
+        val str2Array = IntArray(26)
+        s2.substring(i, i + s1.length).forEach {
+            str2Array[it - 'a'] += 1
+        }
+        if (str2Array.joinToString() == strArray.joinToString()) return true
     }
     return false
 }
