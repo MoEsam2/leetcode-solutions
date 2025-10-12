a3de7c84a89d815d0e7fcdbbc186a619f0c2dac9
Mohamed Esam <moesam138@gmail.com>
2025-10-12 22:30:52 +0300

    [567] [Time Beats: 60.38%] [Memory Beats: 53.01%] - LeetPush

--- sliding-window/Permutation-in-String.kt
+++ sliding-window/Permutation-in-String.kt
@@ -1,16 +1,18 @@
 class Solution {
 fun checkInclusion(s1: String, s2: String): Boolean {
+    if (s1.length > s2.length) return false
     val strArray = IntArray(26)
-    s1.forEach {
-        strArray[it - 'a'] += 1
+    val str2window = IntArray(26)
+    for (i in s1.indices) {
+        strArray[s1[i] - 'a']++
+        str2window[s2[i] - 'a']++
     }
-    for (i in 0..s2.length - s1.length) {
-        val str2Array = IntArray(26)
-        s2.substring(i, i + s1.length).forEach {
-            str2Array[it - 'a'] += 1
-        }
-        if (str2Array.joinToString() == strArray.joinToString()) return true
+    for (i in 0 until s2.length - s1.length) {
+        if (strArray.contentEquals(str2window)) return true
+        //if not the sliding window removes element from left and adds one right
+        str2window[s2[i] - 'a']--
+        str2window[s2[i + s1.length] - 'a']++
     }
-    return false
+    return strArray.contentEquals(str2window)
 }
 }
