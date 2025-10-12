c8f42b50a80c328fe350a1f5edd062c683206fd4
Mohamed Esam <moesam138@gmail.com>
2025-10-12 21:17:53 +0300

    [424] [Time Beats: 33.82%] [Memory Beats: 33.82%] - LeetPush

--- NeetCode 150/Longest-Repeating-Character-Replacement.kt
+++ NeetCode 150/Longest-Repeating-Character-Replacement.kt
@@ -1,21 +1,18 @@
 class Solution {
-   fun characterReplacement(s: String, k: Int): Int {
+fun characterReplacement(s: String, k: Int): Int {
     val seen = hashMapOf<Char, Int>()
     var start = 0
-    var maxFreq = 0
     var max = 0
-    for (end in 0 until s.length) {
-        val currentChar = s[end]
-        seen[currentChar] = seen.getOrDefault(currentChar, 0) + 1
-        maxFreq = if (maxFreq > seen[currentChar]!!) maxFreq else seen[currentChar]!!
-        if ((end - start + 1 )- k > maxFreq) {
+    var maxFreq = 0
+    for ((end, c) in s.withIndex()) {
+        seen[c] = seen.getOrDefault(c, 0) + 1
+        maxFreq = if (maxFreq > seen[c]!!) maxFreq else seen[c]!!
+        if ((end - start + 1) - k > maxFreq) {
             seen[s[start]] = seen.getOrDefault(s[start], 0) - 1
             start++
         }
         max = if (max > end - start + 1) max else end - start + 1
-        println(max)
     }
-
     return max
 }
 }
