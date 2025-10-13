077e787e9c7fca71d72d4c9844acb0844170ad08
Mohamed Esam <moesam138@gmail.com>
2025-10-14 02:03:19 +0300

    [238] [Time Beats: 64.23%] [Memory Beats: 28.26%] - LeetPush

--- Arrays & Hashing/Product-of-Array-Except-Self.kt
+++ Arrays & Hashing/Product-of-Array-Except-Self.kt
@@ -1,8 +1,8 @@
 class Solution {
 fun productExceptSelf(nums: IntArray): IntArray {
-    var prefixProduct = IntArray(nums.size)
-    var suffixProduct = IntArray(nums.size)
-    var productExceptSelf = IntArray(nums.size)
+    val prefixProduct = IntArray(nums.size)
+    val suffixProduct = IntArray(nums.size)
+    val productExceptSelf = IntArray(nums.size)
     for (i in nums.indices) {
         if (i == 0) prefixProduct[i] = nums[i]
         else prefixProduct[i] = nums[i] * prefixProduct[i - 1]
@@ -12,9 +12,11 @@ fun productExceptSelf(nums: IntArray): IntArray {
         else suffixProduct[i] = nums[i] * suffixProduct[i + 1]
     }
     for (i in nums.indices) {
-        if (i == 0) productExceptSelf[i] = suffixProduct[i + 1]
-        else if (i == nums.size - 1) productExceptSelf[i] = prefixProduct[i - 1]
-        else productExceptSelf[i] = prefixProduct[i - 1] * suffixProduct[i + 1]
+       when(i) {
+           0 -> productExceptSelf[i] = suffixProduct[i + 1]
+           nums.size - 1 -> productExceptSelf[i] = prefixProduct[i - 1]
+           else -> productExceptSelf[i] = prefixProduct[i - 1] * suffixProduct[i + 1]
+       }
     }
     return productExceptSelf
 }
