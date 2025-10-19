9aea00e8a44ef838571d087dbad6442691423c01
Mohamed Esam <moesam138@gmail.com>
2025-10-19 21:39:06 +0300

    [3718] [Time Beats: 53.33%] [Memory Beats: 100.00%] - LeetPush

--- Smallest-Missing-Multiple-of-K.kt
+++ Smallest-Missing-Multiple-of-K.kt
@@ -3,7 +3,7 @@ fun missingMultiple(nums: IntArray, k: Int): Int {
     var counter = k
     while (counter <= 10000) {
         if (counter % k == 0 && counter !in nums) return counter
-        else counter++
+        else counter = counter +k
     }
     return 0
 }
