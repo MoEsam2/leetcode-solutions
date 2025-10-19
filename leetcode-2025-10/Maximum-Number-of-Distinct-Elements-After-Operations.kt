c8a1be92ae370a91a135cae70d98e917440face7
Mohamed Esam <moesam138@gmail.com>
2025-10-19 03:08:29 +0300

    [3397] [Time Beats: 100.00%] [Memory Beats: -%] - LeetPush

--- leetcode-2025-10/Maximum-Number-of-Distinct-Elements-After-Operations.kt
+++ leetcode-2025-10/Maximum-Number-of-Distinct-Elements-After-Operations.kt
@@ -1,15 +1,17 @@
 class Solution {
 fun maxDistinctElements(nums: IntArray, k: Int): Int {
     nums.sort()
-    var next = Int.MIN_VALUE
-    var count = 0
-    for (num in nums) {
-        next = maxOf(next, num - k)
-        if (next <= num + k) {
+    var curr = nums[0] - k
+    var count = 1
+    curr++
+    for (i in 1 until nums.size) {
+        curr = if (curr > nums[i] - k) curr else nums[i] - k
+        if (curr <= k + nums[i]) {
             count++
-            next++
+            curr++
         }
     }
     return count
 }
+
 }
