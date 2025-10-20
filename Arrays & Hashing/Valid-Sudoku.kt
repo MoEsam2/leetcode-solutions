8cd76a94f40c5ed1525fe7e8723f68f73b27ca72
Mohamed Esam <moesam138@gmail.com>
2025-10-20 13:05:40 +0300

    [36] [Time Beats: 50.36%] [Memory Beats: 89.10%] - LeetPush

--- Arrays & Hashing/Valid-Sudoku.kt
+++ Arrays & Hashing/Valid-Sudoku.kt
@@ -1,26 +1,14 @@
 class Solution {
 fun isValidSudoku(board: Array<CharArray>): Boolean {
+    val seen = hashSetOf<String>()
     for (i in 0..8) {
-        val rowMap = hashMapOf<Char, Int>()
-        val columMap = hashMapOf<Char, Int>()
-        val boxMap = hashMapOf<Char, Int>()
         for (j in 0..8) {
-            if (board[i][j] != '.') {
-                if (rowMap.containsKey(board[i][j])) {
-                    println("hi")
-                    return false
-                }
-                else rowMap[board[i][j]] = 1
-            }
-            if (board[j][i] != '.') {
-                if (columMap.containsKey(board[j][i])) return false
-                else columMap[board[j][i]] = 1
-            }
-            val rowIndex = 3 * (i / 3) + (j / 3)
-            val columIndex = 3 * (i % 3) + (j % 3)
-            if (board[rowIndex][columIndex] != '.') {
-                if (boxMap.containsKey(board[rowIndex][columIndex])) return false
-                else boxMap[board[rowIndex][columIndex]] = 1
+            val currentValue = board[i][j]
+            if (currentValue != '.') {
+                if (!seen.add("$currentValue found in row $i") ||
+                    !seen.add("$currentValue found in column $j") ||
+                    !seen.add("$currentValue found in box ${i / 3}-${j / 3}")
+                ) return false
             }
         }
     }
