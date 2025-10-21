c92f0c71b5121482ca8e89e4e51505ecb253b1cc
Mohamed Esam <moesam138@gmail.com>
2025-10-21 21:46:09 +0300

    [1004] [Time Beats: 89.10%] [Memory Beats: 51.50%] - LeetPush

--- sliding-window/Max-Consecutive-Ones-III.kt
+++ sliding-window/Max-Consecutive-Ones-III.kt
@@ -1,12 +1,12 @@
 class Solution {
-    fun longestOnes(nums: IntArray, k: Int): Int {
-    val seen = hashMapOf<Int, Int>()
+fun longestOnes(nums: IntArray, k: Int): Int {
     var start = 0
+    var counter = 0
     var max = 0
     for ((end, num) in nums.withIndex()) {
-        seen[num] = seen.getOrDefault(num, 0) + 1
-        if (seen.getOrDefault(0, 0) > k) {
-            seen[nums[start]] = seen.getOrDefault(nums[start], 0) - 1
+        if (nums[end] == 0) counter++
+        if (counter > k) {
+            if (nums[start] == 0) counter--
             start++
         } else {
             max = if (max > end - start + 1) max else end - start + 1
