994cc33932b45077be179864607b04e5a3cacad8
Mohamed Esam <moesam138@gmail.com>
2025-10-23 00:56:39 +0300

    [3719] [Time Beats: 56.67%] [Memory Beats: 60.00%] - LeetPush

--- Longest-Balanced-Subarray-I.kt
+++ Longest-Balanced-Subarray-I.kt
@@ -1,21 +1,21 @@
 class Solution {
-    fun longestBalanced(nums: IntArray): Int {
-        var res = 0
-        for (i in nums.indices) {
-            val even = HashSet<Int>()
-            val odd = HashSet<Int>()
-            var size = 0
-            for (j in i until nums.size) {
-                size++
-                if (nums[j] % 2 == 0)
-                    even.add(nums[j])
-                else
-                    odd.add(nums[j])
+fun longestBalanced(nums: IntArray): Int {
+    var res = 0
+    for (i in nums.indices) {
+        val even = HashSet<Int>()
+        val odd = HashSet<Int>()
+        var size = 0
+        for (j in i until nums.size) {
+            size++
+            if (nums[j] % 2 == 0)
+                even.add(nums[j])
+            else
+                odd.add(nums[j])
 
-                if (even.size == odd.size)
-                    res = maxOf(res, size)
-            }
+            if (even.size == odd.size)
+                res = if(size > res) size else res
         }
-        return res
     }
+    return res
+}
 }
