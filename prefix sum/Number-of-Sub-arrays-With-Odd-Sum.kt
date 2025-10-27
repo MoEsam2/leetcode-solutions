14465e6fa286f3d3bd0ca59c88c2a6ca59170d41
Mohamed Esam <moesam138@gmail.com>
2025-10-27 13:46:24 +0300

    [1524] [Time Beats: 100.00%] [Memory Beats: 66.67%] - LeetPush

---
+++ prefix sum/Number-of-Sub-arrays-With-Odd-Sum.kt
@@ -0,0 +1,20 @@
+class Solution {
+fun numOfSubarrays(arr: IntArray): Int {
+    val MOD = 1_000_000_007
+    var evenCount = 1
+    var oddCount = 0
+    var prefixSum = 0
+    var result = 0
+    for (num in arr) {
+        prefixSum += num
+        if (prefixSum % 2 == 0) {
+            result = (result + oddCount) % MOD
+            evenCount++
+        }else{
+            result = (result + evenCount) %MOD
+            oddCount++
+        }
+    }
+    return result
+}
+}
