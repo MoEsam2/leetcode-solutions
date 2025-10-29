dca7c723b8278565ef15008d76cda2ac4cc2173b
Mohamed Esam <moesam138@gmail.com>
2025-10-30 02:44:28 +0300

    [206] [Time Beats: 100.00%] [Memory Beats: 69.58%] - LeetPush

---
+++ linked_list/Reverse-Linked-List.kt
@@ -0,0 +1,24 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    
+fun reverseList(head: ListNode?): ListNode? {
+    var prev: ListNode? = null
+    var current = head
+    var next: ListNode? = null
+    while (current != null) {
+        next = current.next
+        current.next = prev
+        prev = current
+        current = next
+    }
+    return prev
+}
+}
