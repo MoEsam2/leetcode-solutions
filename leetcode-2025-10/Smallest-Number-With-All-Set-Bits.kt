e42a730dcc7632650c20cd6a37f08775c8a85a8d
Mohamed Esam <moesam138@gmail.com>
2025-10-30 01:20:22 +0300

    [3370] [Time Beats: 100.00%] [Memory Beats: 42.86%] - LeetPush

--- leetcode-2025-10/Smallest-Number-With-All-Set-Bits.kt
+++ leetcode-2025-10/Smallest-Number-With-All-Set-Bits.kt
@@ -1,20 +1,4 @@
 class Solution {
-    fun calculatePower(base: Int, exponent: Int): Int {
-    var result = 1
-    var exp = exponent
-    while (exp != 0) {
-        result *= base
-        exp--
-    }
-    return result
-}
-   fun smallestNumber(n: Int): Int {
-        for (i in 0..10){
-            val result = calculatePower(2,i)-1
-            if(n <= result ){
-                return result
-            }
-        }
-    return 0
-}
+     inline fun smallestNumber(n: Int): Int = (n.takeHighestOneBit() shl 1) - 1
+
 }
