95ad33830a99517b2a397c27c206be3156b23f74
Mohamed Esam <moesam138@gmail.com>
2025-11-06 12:53:24 +0200

    [19] [Time Beats: 100.00%] [Memory Beats: 88.07%] - LeetPush

--- linked_list/Remove-Nth-Node-From-End-of-List.kt
+++ linked_list/Remove-Nth-Node-From-End-of-List.kt
@@ -9,31 +9,17 @@
  */
 class Solution {
 fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
-    var size = getLinkedListSize(head)
-    val dummy = ListNode(0).apply { next = head }
-    var prev = dummy
-    var current = head
-    var counter = 0
-    while (current != null) {
-        var indexFromEnd = size - counter
-        if (indexFromEnd == n) {
-            prev.next = current.next
-        } else {
-            prev = current
-        }
-        counter++
-        current = current.next
+    var fast = head
+    var slow = head
+    repeat(n){
+        fast = fast?.next
     }
-    return dummy.next
-}
-
-fun getLinkedListSize(head: ListNode?): Int {
-    var current = head
-    var size = 0
-    while (current != null) {
-        size++
-        current = current.next
+    if(fast == null) return head?.next
+    while (fast?.next != null){
+        fast = fast?.next
+        slow = slow?.next
     }
-    return size
+    slow?.next = slow?.next?.next
+    return head
 }
 }
