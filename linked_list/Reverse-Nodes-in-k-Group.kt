891e248e0f5af4f6de4cadb376dd431dde53f0b4
Mohamed Esam <moesam138@gmail.com>
2025-11-19 16:35:07 +0200

    [25] [Time Beats: 74.48%] [Memory Beats: 76.55%] - LeetPush

---
+++ linked_list/Reverse-Nodes-in-k-Group.kt
@@ -0,0 +1,46 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+   fun reverseKGroup(head: ListNode?, k: Int): ListNode? {
+    val dummy = ListNode(0)
+    var tail = dummy
+    var current = head
+    var index = 1
+    var next: ListNode? = null
+    var start = head
+    while (current != null) {
+        while (current != null && index != k) {
+            current = current.next
+            if (current == null) break
+            index++
+        }
+        if (index != k){
+         tail.next = next
+         break
+        }
+        next = current?.next
+        var prev: ListNode? = null
+        var currentShift = start
+        var nextShift = start?.next
+        while (currentShift != next) {
+            currentShift?.next = prev
+            prev = currentShift
+            currentShift = nextShift
+            nextShift = nextShift?.next
+        }
+        tail.next = prev
+        tail = start!!
+        start = next
+        current = next
+        index = 1
+    }
+    return dummy.next
+}
+}
