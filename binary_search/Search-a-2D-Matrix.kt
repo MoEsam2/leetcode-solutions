c8eb3efc37fb28ff35b2a26689600ee531623b5e
Mohamed Esam <moesam138@gmail.com>
2025-12-29 08:22:11 +0200

    [74] [Time Beats: 100.00%] [Memory Beats: 44.85%] - LeetPush

--- binary_search/Search-a-2D-Matrix.kt
+++ binary_search/Search-a-2D-Matrix.kt
@@ -1,20 +1,32 @@
-class Solution {
-fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
-    val m = matrix.size
-    val n = matrix[0].size
-    var left = 0
-    var right = m * n - 1
-    while (left <= right) {
-        val mid = left + (right - left) / 2
-        val row = mid / n
-        val col = mid % n
-        val value = matrix[row][col]
-        when {
-            value == target -> return true
-            value < target -> left = mid + 1
-            else -> right = mid - 1
-        }
-    }
-    return false
-}
-}
+1class Solution {
+2fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+3    val arr = FlattenedMatrix(matrix)
+4    var left = 0
+5    var right = arr.size - 1
+6    while (left <= right) {
+7        val middle = left + (right - left) / 2
+8        when {
+9            arr[middle] == target -> return true
+10            arr[middle] < target -> left = middle + 1
+11            else -> right = middle - 1
+12        }
+13    }
+14    return false
+15
+16}
+17
+18private fun FlattenedMatrix(matrix: Array<IntArray>): IntArray {
+19    val rows = matrix.size
+20    val cols = matrix[0].size
+21    val array = IntArray(rows * cols)
+22
+23    var index = 0
+24    for (i in 0 until rows) {
+25        for (j in 0 until cols) {
+26            array[index] = matrix[i][j]
+27            index++
+28        }
+29    }
+30    return array
+31}
+32}
